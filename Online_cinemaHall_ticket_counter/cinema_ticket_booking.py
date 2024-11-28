ab353fb7b8923e9529f9fed26b7379a714c80b81
phitron-demo <sojibhasan5800@gmail.com>
2024-11-28 09:08:15 +0600

    kaj

--- Online_cinemaHall_ticket_counter/cinema_ticket_booking.py
+++ Online_cinemaHall_ticket_counter/cinema_ticket_booking.py
@@ -24,11 +24,11 @@ class Star_Cinema:
         return cls.__view_hall_show_list
     
     #check duplicated same Hall are hall No exited My Optional use
-    __hall_no_store={}
+    __hall_no_store=[]
     
     @classmethod
     def _hall_no_valueSet(cls,value):
-        cls.__hall_no_store[value]=True
+        cls.__hall_no_store.append(value)
 
     @classmethod
     def get_hall_no_store(cls):
@@ -66,21 +66,22 @@ class Star_Cinema:
 #Create Hall class and taken three parameter
 
 class Hall(Star_Cinema):
+
     def __init__(self, rows:int, cols:int, hall_no:int):
+        
+        if hall_no in Star_Cinema.get_hall_no_store():
+            # raise ValueError(f"Error: Hall No. {hall_no} is already booked.")
+            print(f"Error: Hall No. {hall_no} is already booked.")
+            self.__valid=False
+            
+        if  getattr (self,"__valid",False):
+            pass
+        else:
+
+            Star_Cinema._hall_no_valueSet(hall_no)
+
 
         
-        #Check duplicated hall no
-        check =False
-        for key, val in Star_Cinema.get_hall_no_store().items():
-            if(key==hall_no):
-                check=True
-                break
-
-        if(check==True):
-            print(f"This hall No: {hall_no} already admin booked")
-            return
-        
-        Star_Cinema._hall_no_valueSet(hall_no)
            
             
         
@@ -92,26 +93,36 @@ class Hall(Star_Cinema):
         self.__rows = rows 
         self.__cols = cols 
         self.__hall_no = hall_no 
-
+           
         #added entery hall for passing hall_list
         Star_Cinema.entry_hall(self)
 
 
       # This show are entery in hall class init method are added that private __show_list
+      
       #-----Only Admin Changes-----
     def entry_show(self, id:str, movie_name:str, time:str):
 
-        show_info = (id, movie_name, time) 
-        self.__show_list.append(show_info)
-        view_info = f"Hall {id}: Movie '{movie_name}' at {time} (Show ID: {id})"
-        Star_Cinema._add_hall_show_list(view_info)
+        # Prevent operations on an invalid instance
+        if not getattr(self,"__valid",True):
+            print("Error: Cannot add a show to an invalid hall instance.")
+
+        else:           
+
+            show_info = (id, movie_name, time) 
+        
+            self.__show_list.append(show_info)
+
+            view_info = f"Hall {id}: Movie '{movie_name}' at {time} (Show ID: {id})"
+            Star_Cinema._add_hall_show_list(view_info)
 
         # attach hall_no depend add the set of hall class
-        seat_layout = [[0 for _ in range(self.__cols)] for _ in range(self.__rows)]
-        self.__seats[id] = seat_layout  
+            seat_layout = [[0 for _ in range(self.__cols)] for _ in range(self.__rows)]
+            self.__seats[id] = seat_layout  
 
 
         #Anyone set book of the depanded mv id of entry_show
+
     def book_seats(self,user_mv_id:str,seat_number:tuple):
         if user_mv_id not in self.__seats:
             print("The Movie Id did not match anyone place check id and try again latter!")
@@ -152,12 +163,56 @@ class Hall(Star_Cinema):
 kaji_hall=Hall(10,10,250)
 kaji_hall.entry_show("112","Robot3.0",datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 kaji_hall=Hall(10,10,350)
-kaji_hall.entry_show("112","Robot3.0",datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+kaji_hall.entry_show("112","IronMan5.0",datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 kaji_hall=Hall(10,10,350)
-kaji_hall.entry_show("112","Robot3.0",datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+kaji_hall.entry_show("112","Superman",datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
-savana_hall=Hall(10,10,250)
+savana_hall=Hall(10,10,2550)
 savana_hall.entry_show("115","AmmJan",datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+#----------------------- Close Authority Function ----------------------------------
+
+#----------------------- User Given Data Checking ----------------------------------
+def Cheking_User_data(x):
+
+
+
+
+#-----------User Display Data given------------
+
+while True:
+    print("(1) Enter View Hall: ")
+    print("(2) Enter Movie Id & Show Avaiable seat: ")
+    print("(3) Enter Movie Id & Book seat: ")
+    print("(4) Exit: ")
+    x = input()
+    if( Cheking_User_data(x)):
+        pass
+    else:
+        continue
+
+    if  x.isalpha():
+        print(f"This {x} is alpha Character Place given integer Number !")
+        continue
+    elif x.isnumeric():
+        print(f"This {x} is Numeric number Place given integer Number !")
+        continue
+    else:
+        x=int(x)
+        if not (x>=1 and x<=4):
+            print(f"Place Sir given This Number (1 to 4) are inclusive")
+            continue
+        else:
+            if(x==1):
+                #Enter view Hall---->
+                for hall_Movie_details in Star_Cinema.view_halls():
+                    print(hall_Movie_details)
+                print("(2) Enter Movie Id & Show Avaiable seat: ")
+                print("(3) Enter Movie Id & Book seat: ")
+                print("(4) Exit: ")
+                
+
+        
+
 
 
 
